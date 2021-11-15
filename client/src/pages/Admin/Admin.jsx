b8ade054f12c8a840882352c66d57fb07d3f0de3
Gandha-PC <ganzs499@gmail.com>
2021-11-15 08:04:29 +0700

    Update: Add Fitur Modal / Zoom / Lightbox / Image Transaction Admin

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react'
-
-import './Admin.css'
-import { Table, Dropdown, Modal, Container } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import './Admin.css';
+import { Table, Dropdown, Container } from 'react-bootstrap'
 import NavbarAdmin from '../../components/NavbarAdmin/NavbarAdmin';
-
+import ModalImage from 'react-modal-image';
 import { API } from '../../config/api/api';
 
 import './Admin.css';
@@ -11,18 +10,6 @@ import './Admin.css';
 const Admin = () => {
 
     const [transactions, setTransactions] = useState([]);
-    const [isDialog, setIsDialog] = useState(false)
-    const [activeObject, setActiveObject] = useState(null)
-
-    function getClass(index) {
-        return index === activeObject?.id ? "active" : "inactive"
-    }
-
-    const ModalTransaction = ({ object: { image } }) => (
-        <Modal className="active">
-            <img src={image} alt="view" />
-        </Modal>
-    )
 
     const getTransactions = async () => {
         try {
@@ -71,14 +58,6 @@ const Admin = () => {
         }
     }
 
-    const handleShowDialog = () => {
-        setIsDialog({ isDialog: !isDialog })
-    }
-
-    // const handleCloseDialog = () => {
-    //     setIsDialog(false)
-    // }
-
 
     useEffect(() => {
         getTransactions();
@@ -109,39 +88,19 @@ const Admin = () => {
                             <td className="table-cell align-middle">{index + 1}</td>
                             <td className="table-cell align-middle">{item?.user?.name}</td>
                             <td className="table-cell align-middle">
-                                {item?.transferProof === "http://localhost:5000/uploads/-" ? (
-                                    <div style={{ marginTop: "15px" }}>
-                                        <p>No Transactions</p>
-                                    </div>
-                                ) : (
-                                    <div>
-                                        <img
-                                            key={item?.id}
-                                            className={getClass(item?.id)}
-                                            onClick={handleShowDialog}
-                                            src={item?.transferProof ? item?.transferProof : null}
-                                            alt="view"
-                                            style={{ width: "150px", cursor: "pointer" }}
-                                        />
-                                        {isDialog && (
-                                            <dialog
-                                                className="dialog"
-                                                style={{ position: "absolute", left: "500px" }}
-                                                open
-                                                onClick={handleShowDialog}
-                                            >
-                                                <img
-                                                    key={item?.id}
-                                                    className={getClass(item?.id)}
-                                                    onClick={handleShowDialog}
-                                                    src={item?.transferProof ? item?.transferProof : null}
-                                                    alt="view"
-                                                    style={{ width: "300px", cursor: "pointer" }}
-                                                />
-                                            </dialog>
-                                        )}
-                                    </div>
-                                )
+                                {item?.transferProof === "http://localhost:5000/uploads/-"
+                                    ? (
+                                        <div style={{ marginTop: "15px" }}>
+                                            <p>No Transactions</p>
+                                        </div>
+                                    ) : (
+                                        <div className="image-transaction-wrapper">
+                                            <ModalImage
+                                                small={item?.transferProof}
+                                                large={item?.transferProof}
+                                            />
+                                        </div>
+                                    )
                                 }
                             </td>
                             <td className="table-cell align-middle">{item?.remainingActive} / Hari</td>
